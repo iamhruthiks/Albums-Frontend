6ac68835b404623bd9b696208867fabc862db273
iamhruthiks <iamhruthiks@gmail.com>
2025-03-27 15:02:18 +0530

    Section 61: Section 04 - Albums use cases in React - 501. Albums Show page, Network calls

--- src/client/client.js
+++ src/client/client.js
@@ -71,5 +71,22 @@ const fetchPostFileUploadWithAuth = async (uri, formData) => {
   }
 };
 
+const fetchGetDataWithAuthArrayBuffer = (uri) => {
+  const token = localStorage.getItem('token');
+  const url = `${API_version}${uri}`;
+  try {
+    const response = axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      responseType: 'arraybuffer'
+    });
+    return response;
+  } catch (error) {
+    // Handle errors if the request fails
+    console.error('Error fetching data:', error);
+  }
+};
+
 export default fetchGetData;
-export { fetchPostData, fetchPostDataWithAuth, fetchGetDataWithAuth, fetchPostFileUploadWithAuth };
+export { fetchPostData, fetchPostDataWithAuth, fetchGetDataWithAuth, fetchPostFileUploadWithAuth, fetchGetDataWithAuthArrayBuffer };
--- src/pages/albums/albums/photoGrid.js
+++ src/pages/albums/albums/photoGrid.js
@@ -1,31 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Grid, Typography, Tooltip } from '@mui/material';
+import { fetchGetDataWithAuthArrayBuffer, fetchGetDataWithAuth } from 'client/client';
+import { Buffer } from 'buffer';
+import { useLocation } from 'react-router-dom';
 
-const PhotoGrid = ({ photoUrls }) => {
-  const [photos, setPhotos] = useState(new Set());
+const PhotoGrid = () => {
+  const [photos, setPhotos] = useState({}); // State to store fetched photos
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const album_id = queryParams.get('id'); // Extract album ID from URL query parameters
+
   useEffect(() => {
-    const addPhotoAsync = async (link) => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      setPhotos((prevPhotos) => new Set([...prevPhotos, link]));
-    };
-
-    const loadPhotos = async () => {
-      for (const link of photoUrls) {
-        await addPhotoAsync(link);
-      }
-    };
-    loadPhotos();
-  }, []);
-
+    // Fetch album data when component mounts or album_id changes
+    fetchGetDataWithAuth('/albums/' + album_id).then((res) => {
+      console.log('res', res.data);
+      const photosList = res.data.photos; // Extract list of photos from album data
+      // Fetch and update photos one by one as they are downloaded
+      photosList.forEach((photo) => {
+        // Fetch individual photo data
+        fetchGetDataWithAuthArrayBuffer(photo.download_link).then((response) => {
+          // Construct a unique ID for the photo
+          const albumPhotoID = 'album_' + album_id + '_photo+' + photo.id;
+          // Convert photo data to base64 format
+          const buffer = Buffer.from(response.data, 'binary').toString('base64');
+          // Update state with the fetched photo
+          const temp = {
+            album_id: album_id,
+            photo_id: photo.id,
+            name: photo.name,
+            description: photo.desciption,
+            content: buffer
+          };
+          setPhotos((prevPhotos) => ({ ...prevPhotos, [albumPhotoID]: temp }));
+        });
+      });
+    });
+  }, [album_id]); // Dependency array ensures useEffect runs when album_id changes
   return (
     <Grid container spacing={2}>
-      {[...photos].map((photo) => (
-        <Grid item key={photo.id} xs={8} sm={4} md={4} lg={2}>
+      {/* Render each photo */}
+      {Object.keys(photos).map((key) => (
+        <Grid item key={key} xs={8} sm={4} md={4} lg={2}>
           <Card>
-            <CardMedia component="img" height="200" image={photo.url} alt={photo.title} />
+            <Tooltip title={photos[key]['description']}>
+              <CardMedia
+                component="img"
+                height="200"
+                image={'data:image/jpeg;base64,' + photos[key]['content']}
+                alt={photos[key]['description']}
+                title={photos[key]['description']}
+              />
+            </Tooltip>
             <CardContent>
-              <Typography variant="subtitle1">{photo.title}</Typography>
+              <Tooltip title={photos[key]['description']}>
+                <Typography variant="subtitle1">{photos[key]['name']}</Typography>
+              </Tooltip>
             </CardContent>
           </Card>
         </Grid>
