d3a75515ae705c493d44e2bcfbacba9fab329826
iamhruthiks <iamhruthiks@gmail.com>
2025-03-27 13:48:36 +0530

    Section 61: Section 04 - Albums use cases in React - 498. Upload photos form

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -9,6 +9,7 @@ const AlbumsPage = Loadable(lazy(() => import('pages/albums/albums')));
 const AboutPage = Loadable(lazy(() => import('pages/staticPages/about')));
 const AlbumAddPage = Loadable(lazy(() => import('pages/albums/albumAdd')));
 const AlbumShowPage = Loadable(lazy(() => import('pages/albums/albumShow')));
+const AlbumUploadPage = Loadable(lazy(() => import('pages/albums/albumUpload')));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
@@ -31,6 +32,10 @@ const MainRoutes = {
     {
       path: '/album/show',
       element: <AlbumShowPage />
+    },
+    {
+      path: '/album/upload',
+      element: <AlbumUploadPage />
     }
   ]
 };
