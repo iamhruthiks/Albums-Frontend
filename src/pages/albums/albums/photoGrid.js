3925ac4ce19a4a17606f33ad5bc536d77ed4b7c5
iamhruthiks <iamhruthiks@gmail.com>
2025-03-27 20:13:18 +0530

    Section 61: Section 04 - Albums use cases in React - 503. Add album and photo actions

--- src/pages/albums/albums/photoGrid.js
+++ src/pages/albums/albums/photoGrid.js
@@ -10,6 +10,22 @@ const PhotoGrid = () => {
   const queryParams = new URLSearchParams(location.search);
   const album_id = queryParams.get('id'); // Extract album ID from URL query parameters
 
+  const handleView = () => {
+    console.log('View clicked!');
+  };
+
+  const handleEdit = () => {
+    console.log('Edit clicked!');
+  };
+
+  const handleDownload = () => {
+    console.log('Download clicked!');
+  };
+
+  const handleDelete = () => {
+    console.log('Delete clicked!');
+  };
+
   useEffect(() => {
     // Fetch album data when component mounts or album_id changes
     fetchGetDataWithAuth('/albums/' + album_id).then((res) => {
@@ -55,6 +71,21 @@ const PhotoGrid = () => {
               <Tooltip title={photos[key]['description']}>
                 <Typography variant="subtitle1">{photos[key]['name']}</Typography>
               </Tooltip>
+              <a href="#" onClick={handleView}>
+                View
+              </a>
+              |
+              <a href="#" onClick={handleEdit}>
+                Edit
+              </a>
+              |
+              <a href="#" onClick={handleDownload}>
+                Download
+              </a>
+              |
+              <a href="#" onClick={handleDelete}>
+                Delete
+              </a>
             </CardContent>
           </Card>
         </Grid>
