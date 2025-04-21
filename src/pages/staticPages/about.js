044f57bd35f2c310626149530bfc4841d9087064
iamhruthiks <iamhruthiks@gmail.com>
2025-04-21 13:38:27 +0530

    update project title and meta tags

--- src/pages/staticPages/about.js
+++ src/pages/staticPages/about.js
@@ -5,7 +5,10 @@ import MainCard from 'components/MainCard';
 const SamplePage = () => {
   return (
     <MainCard title="Albums">
-      <Typography variant="body2">About React!</Typography>
+      <Typography variant="body2">
+        Albums App is a full-stack web application built using Spring Boot and React. It allows users to manage personal photo albums with
+        features like creating albums, uploading photos, editing or deleting them, and even downloading photos.
+      </Typography>
     </MainCard>
   );
 };
